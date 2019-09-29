ca88c3358ad700792d8f2f19e1ab1280d61bcb93
David Walther <david.walther@cloudideas.com>
2019-09-29 17:20:56 +0200

    set up basic app with direct timestamp output

--- src/modules/app/timeTracking/timeTracking.js
+++ src/modules/app/timeTracking/timeTracking.js
@@ -1,3 +1,89 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import { Storage } from 'data/localStorage';
 
-export default class TimeTracking extends LightningElement {}
+export default class TimeTracking extends LightningElement {
+    timestamps = [];
+    @track entryPairs = [];
+
+    connectedCallback() {
+        this.loadData();
+    }
+
+    refreshTimePairList() {
+        this.entryPairs = [];
+        for (let index = 0; index < this.timestamps.length; index++) {
+            const element = this.timestamps[index];
+            if (index % 2 === 0) {
+                let newEntry = {
+                    id: this.entryPairs.length,
+                    start: element.timeStamp
+                };
+                this.entryPairs.push(newEntry);
+            }
+            if (index % 2 === 1) {
+                let entry = this.entryPairs[this.entryPairs.length - 1];
+                entry.end = element.timeStamp;
+            }
+        }
+    }
+
+    handleCick() {
+        let timeStamp = new Date();
+        let id = this.timestamps === null ? 0 : this.timestamps.length;
+
+        var obj = {
+            timeStamp: timeStamp.getTime(),
+            id: id
+        };
+
+        this.timestamps.push(obj);
+        this.refreshTimePairList();
+    }
+
+    handleLoadBtnClick() {
+        this.loadData();
+    }
+
+    handleSaveBtnClick() {
+        this.saveData();
+    }
+
+    handleClearBtnClick() {
+        this.clearData();
+        this.refreshTimePairList();
+    }
+
+    clearData() {
+        var storage = new Storage();
+
+        this.timestamps = [];
+        storage.clear();
+    }
+
+    saveData() {
+        var storage = new Storage();
+        storage.save(this.timestamps);
+    }
+
+    loadData() {
+        var storage = new Storage();
+        this.timestamps = storage.load();
+        if (this.timestamps === null) {
+            this.timestamps = [];
+        }
+        this.refreshTimePairList();
+    }
+
+    handleChangeTimestamp(event) {
+        var identifier, newValue;
+
+        identifier = event.detail.identifier;
+        newValue = event.detail.newValue;
+
+        let keys = identifier.split('-');
+
+        let timestampindex = keys[0] * 2 + (keys[1] === 'start' ? 0 : 1);
+
+        this.timestamps[timestampindex].timeStamp = newValue;
+    }
+}
