c8f0622f60fae377d7eea1ec2676683a49166fc2
David Walther <david.walther@cloudideas.com>
2019-10-12 14:25:11 +0200

    added structural tests

--- src/modules/ui/entry/__tests__/entry.test.js
+++ src/modules/ui/entry/__tests__/entry.test.js
@@ -1,3 +1,15 @@
-describe('', () => {
-    test('', () => {});
+import { createElement } from 'lwc';
+import Entry from 'ui/entry';
+
+describe('static tests', () => {
+    test('test structure', () => {
+        const element = createElement('ui-entry', { is: Entry });
+        document.body.appendChild(element);
+
+        const contentContainer = element.shadowRoot.querySelector(
+            'div.content'
+        );
+
+        expect(contentContainer).toBeTruthy();
+    });
 });
