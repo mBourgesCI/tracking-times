877a641463816036eb2254e293b1becf46f9798c
David Walther <david.walther@cloudideas.com>
2019-10-12 20:25:19 +0200

    repeated for all attributes

--- src/modules/ui/entry/entry.js
+++ src/modules/ui/entry/entry.js
@@ -8,10 +8,38 @@ export default class Entry extends LightningElement {
     set startDate(value) {
         this.state.api.startDate = value;
     }
-    @api startTime;
-    @api endDate;
-    @api endTime;
-    @api comment;
+
+    @api
+    get startTime() {
+        return this.state.api.startTime;
+    }
+    set startTime(value) {
+        this.state.api.startTime = value;
+    }
+
+    @api
+    get endDate() {
+        return this.state.api.endDate;
+    }
+    set endDate(value) {
+        this.state.api.endDate = value;
+    }
+
+    @api
+    get endTime() {
+        return this.state.api.endTime;
+    }
+    set endTime(value) {
+        this.state.api.endTime = value;
+    }
+
+    @api
+    get comment() {
+        return this.state.api.comment;
+    }
+    set comment(value) {
+        this.state.api.comment = value;
+    }
 
     state = { api: {} };
 }
