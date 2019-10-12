79b204e49be96ebe5b7899c1002598832e0bf6ac
David Walther <david.walther@cloudideas.com>
2019-10-12 20:22:20 +0200

    refactured startDate to be a property and to access the state variables.

--- src/modules/ui/entry/entry.js
+++ src/modules/ui/entry/entry.js
@@ -1,11 +1,17 @@
 import { LightningElement, api } from 'lwc';
 
 export default class Entry extends LightningElement {
-    @api startDate;
+    @api
+    get startDate() {
+        return this.state.api.startDate;
+    }
+    set startDate(value) {
+        this.state.api.startDate = value;
+    }
     @api startTime;
     @api endDate;
     @api endTime;
     @api comment;
 
-    state = {api:{}};
+    state = { api: {} };
 }
