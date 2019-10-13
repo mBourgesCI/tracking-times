17c4cdb574906e35b6e9760ae57dbeb4270fe7fa
David Walther <david.walther@cloudideas.com>
2019-10-13 18:53:31 +0200

    checked for input

--- src/modules/ui/jsonCmp/__tests__/jsonCmp.test.js
+++ src/modules/ui/jsonCmp/__tests__/jsonCmp.test.js
@@ -1,4 +1,4 @@
-import createElement from 'lwc';
+import { createElement } from 'lwc';
 import JsonCmp from 'ui/jsonCmp';
 
 describe('check json', () => {
@@ -8,5 +8,8 @@ describe('check json', () => {
         const element = createElement('ui-json-cmp', { is: JsonCmp });
         element.inputJson = jsonInput;
         document.body.appendChild(element);
+
+        const valueInput = element.shadowRoot.querySelector('input');
+        expect(valueInput).toBeTruthy();
     });
 });
