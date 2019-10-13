af8d959f10c3184218d4897dfdd3931af35ccacd
David Walther <david.walther@cloudideas.com>
2019-10-13 19:50:45 +0200

    disect input json and store separat value

--- src/modules/ui/jsonCmp/jsonCmp.js
+++ src/modules/ui/jsonCmp/jsonCmp.js
@@ -3,13 +3,16 @@ import { LightningElement, api, track } from 'lwc';
 export default class JsonCmp extends LightningElement {
     @api
     get inputJson() {
-        return this.state.api.inputJson;
+        return this.state.api.value;
     }
     set inputJson(value) {
-        this.state.api.inputJson = value;
+        this.state.api.value = value.value;
     }
 
-    @track state = { api: {} };
+    @track
+    state = { api: {} };
 
-    handleChamge() {}
+    handleChamge(event) {
+        this.state.api.value = event.target.value;
+    }
 }
