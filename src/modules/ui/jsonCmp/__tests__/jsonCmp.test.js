3c26f321b4dce88e6db37bd9705ab171d8f6d12d
David Walther <david.walther@cloudideas.com>
2019-10-13 18:47:55 +0200

    create json in test and pass to cmp

--- src/modules/ui/jsonCmp/__tests__/jsonCmp.test.js
+++ src/modules/ui/jsonCmp/__tests__/jsonCmp.test.js
@@ -3,7 +3,10 @@ import JsonCmp from 'ui/jsonCmp';
 
 describe('check json', () => {
     test('use json as input', () => {
+        const jsonInput = { value: 'test' };
+
         const element = createElement('ui-json-cmp', { is: JsonCmp });
+        element.inputJson = jsonInput;
         document.body.appendChild(element);
     });
 });
