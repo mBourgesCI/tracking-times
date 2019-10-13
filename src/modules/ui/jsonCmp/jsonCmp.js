e4ebaf355917e40ca0bcde59a031bb28eb02034e
David Walther <david.walther@cloudideas.com>
2019-10-13 18:58:31 +0200

    added attribute to api

--- src/modules/ui/jsonCmp/jsonCmp.js
+++ src/modules/ui/jsonCmp/jsonCmp.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class JsonCmp extends LightningElement {}
+export default class JsonCmp extends LightningElement {
+    @api inputJson;
+}
