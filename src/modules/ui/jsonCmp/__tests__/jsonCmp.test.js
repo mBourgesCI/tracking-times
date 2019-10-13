6a253dac9d54f9465f8478b72459c0a3376951ec
David Walther <david.walther@cloudideas.com>
2019-10-13 18:45:00 +0200

    create cmp and add to "test-DOM"

--- src/modules/ui/jsonCmp/__tests__/jsonCmp.test.js
+++ src/modules/ui/jsonCmp/__tests__/jsonCmp.test.js
@@ -1,3 +1,9 @@
-describe('', () => {
-    test('', () => {});
+import createElement from 'lwc';
+import JsonCmp from 'ui/jsonCmp';
+
+describe('check json', () => {
+    test('use json as input', () => {
+        const element = createElement('ui-json-cmp', { is: JsonCmp });
+        document.body.appendChild(element);
+    });
 });
