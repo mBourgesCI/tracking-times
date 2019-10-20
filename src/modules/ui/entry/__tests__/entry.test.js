3ac2711c7118a05adf133f63b88027582b3c6dea
David Walther <david.walther@cloudideas.com>
2019-10-20 20:19:34 +0200

    switched test of initial calculation

--- src/modules/ui/entry/__tests__/entry.test.js
+++ src/modules/ui/entry/__tests__/entry.test.js
@@ -471,16 +471,13 @@ describe('behavior on change', () => {
     });
 
     test('difference is calculated initialy.', () => {
-        const startDate = '1900-01-01';
-        const startTime = '13:00';
-        const endDate = '1900-01-01';
-        const endTime = '14:00';
+        const jsonInput = {
+            start: { value: new Date('1970-01-01T00:00:00.0000Z').getTime() },
+            end: { value: new Date('1970-01-01T01:00:00.0000Z').getTime() }
+        };
 
         const element = createElement('ui-entry', { is: Entry });
-        element.startDate = startDate;
-        element.startTime = startTime;
-        element.endDate = endDate;
-        element.endTime = endTime;
+        element.jsonInput = jsonInput;
         document.body.appendChild(element);
 
         const diffOutput = element.shadowRoot.querySelector('span.diff');
