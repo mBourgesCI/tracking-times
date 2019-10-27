f8544e00f214db1e22c525949c944680cac3b92a
David Walther <david.walther@cloudideas.com>
2019-10-27 13:54:10 +0100

    added test to prove old data keeps being handled

--- src/modules/app/timeTracking/__tests__/timeTracking.test.js
+++ src/modules/app/timeTracking/__tests__/timeTracking.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { createElement } from 'lwc';
 import TimeTracking from 'app/timeTracking';
-
+/*
 describe('test core logic', () => {
     afterEach(() => {
         // The jsdom instance is shared across test cases in a single file so reset the DOM
@@ -27,5 +27,53 @@ describe('test creation of new entries', () => {
         while (document.body.firstChild) {
             document.body.removeChild(document.body.firstChild);
         }
+    });
+});
+*/
+describe('check loading based on version', () => {
+    function getSaveButton(shadowRoot) {
+        return getButton(shadowRoot, '.button-save');
+    }
+
+    function getAddButton(shadowRoot) {
+        return getButton(shadowRoot, '.button-add');
+    }
+
+    function getLoadButton(shadowRoot) {
+        return getButton(shadowRoot, '.button-load');
+    }
+
+    function getButton(shadowRoot, classname) {
+        const resultList = shadowRoot.querySelectorAll(classname);
+        expect(resultList).toBeTruthy();
+        expect(resultList.length).toBe(1);
+        return resultList[0];
+    }
+
+    test('load data without version', () => {
+        const storageString = [
+            {
+                data:
+                    '{"start":{"value":0},"end":{"value":1000},"comment":"legacy entry","id":0}',
+                index: 0
+            },
+            {
+                data:
+                    '{"start":{"value":0},"end":{"value":1000},"comment":"legacy entry","id":1}',
+                index: 1
+            }
+        ];
+        localStorage.setItem('storage', JSON.stringify(storageString));
+
+        const element = createElement('app-timeTracking', { is: TimeTracking });
+
+        document.body.appendChild(element);
+
+        return Promise.resolve().then(() => {
+            const entries = element.shadowRoot.querySelectorAll('ui-entry');
+            expect(entries).toBeTruthy();
+            expect(entries.length).toBeTruthy();
+            expect(entries.length).toBe(2);
+        });
     });
 });
