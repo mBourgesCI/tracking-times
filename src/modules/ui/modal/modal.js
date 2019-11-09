bf47ae20435d961ca8f3c7545da8811ff67b59c8
David Walther <david.walther@cloudideas.com>
2019-11-09 22:07:17 +0100

    create label object

--- src/modules/ui/modal/modal.js
+++ src/modules/ui/modal/modal.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class Entry extends LightningElement {}
+export default class Entry extends LightningElement {
+    label = { button: { close: 'Close' } };
+}
