02fb1bbc70cc17347e65af5d0ed09ce9a7c2ed84
David Walther <david.walther@cloudideas.com>
2019-11-09 22:48:17 +0100

    create select and hiding logic

--- src/modules/ui/modal/modal.js
+++ src/modules/ui/modal/modal.js
@@ -3,5 +3,21 @@ import { LightningElement } from 'lwc';
 export default class Entry extends LightningElement {
     label = { button: { close: 'Close' } };
 
-    handleButtonClickClose() {}
+    handleButtonClickClose() {
+        this.hideModal();
+    }
+
+    hideModal() {
+        var modalElem;
+        modalElem = this.getModalContainer();
+        this.setElemDisplay(modalElem, 'none');
+    }
+
+    getModalContainer() {
+        return this.template.querySelector('div.modal');
+    }
+
+    setElemDisplay(elem, displayValue) {
+        elem.style.display = displayValue;
+    }
 }
