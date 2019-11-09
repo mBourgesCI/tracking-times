e085d250254ec1f7785450f402f2655ab79753c5
David Walther <david.walther@cloudideas.com>
2019-11-09 23:25:58 +0100

    added show method to api

--- src/modules/ui/modal/modal.js
+++ src/modules/ui/modal/modal.js
@@ -1,6 +1,11 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 
 export default class Entry extends LightningElement {
+    @api
+    show() {
+
+    }
+
     @track
     label = {
         title: 'Head',
