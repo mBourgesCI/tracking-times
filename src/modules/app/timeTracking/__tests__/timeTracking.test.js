e0c62660af12dc430b3d9acc8fd8eb5f0ccae3c9
David Walther <david.walther@cloudideas.com>
2020-01-25 14:38:27 +0100

    Add cleanup method

--- src/modules/app/timeTracking/__tests__/timeTracking.test.js
+++ src/modules/app/timeTracking/__tests__/timeTracking.test.js
@@ -267,6 +267,14 @@ describe('check buttons', () => {
 });
 
 describe('check delete', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+        clearStorage();
+    });
+
     test('check delete', () => {});
 });
 
