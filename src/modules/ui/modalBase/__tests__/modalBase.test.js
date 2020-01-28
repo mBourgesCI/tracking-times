1481186fbd3cd9428ffabc131b35aea2fbaaeafc
David Walther <david.walther@cloudideas.com>
2020-01-28 11:55:11 +0100

    created basic logic

---
+++ src/modules/ui/modalBase/__tests__/modalBase.test.js
@@ -0,0 +1,10 @@
+describe('', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    test('', () => {});
+});
