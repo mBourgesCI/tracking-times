e61719341446354857e9eaa36cb8de1d9fc6e326
David Walther <david.walther@cloudideas.com>
2020-01-28 16:59:55 +0100

    added structure

---
+++ src/modules/ui/modalConfirm/__tests__/modalConfirm.test.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import { createElement } from 'lwc';
+// eslint-disable-next-line no-unused-vars
+import ModalConfirm from 'ui/modalConfirm';
+
+describe('', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    test('', () => {});
+});
