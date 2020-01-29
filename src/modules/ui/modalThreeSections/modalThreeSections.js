920ff02ce4864455f8df5e689280a4056bda6f2d
David Walther <david.walther@cloudideas.com>
2020-01-29 21:46:20 +0100

    added visibility check for three section modal

--- src/modules/ui/modalThreeSections/modalThreeSections.js
+++ src/modules/ui/modalThreeSections/modalThreeSections.js
@@ -1,3 +1,12 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class ModalThreeSections extends LightningElement {}
+export default class ModalThreeSections extends LightningElement {
+    @api
+    isVisible() {
+        return this.getBaseModal().isVisible();
+    }
+
+    getBaseModal() {
+        return this.template.querySelector('.modal-base');
+    }
+}
