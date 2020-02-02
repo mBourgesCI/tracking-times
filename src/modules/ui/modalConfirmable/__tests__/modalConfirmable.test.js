d4244af3807d26d847c8a7b619f69e38ef23f745
David Walther <david.walther@cloudideas.com>
2020-02-02 11:59:19 +0100

    create basic structure

---
+++ src/modules/ui/modalConfirmable/__tests__/modalConfirmable.test.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+
+import { createElement } from 'lwc';
+import ModalConfirmable from 'ui/modalConfirmable';
+
+describe('', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    test('', () => {});
+});
