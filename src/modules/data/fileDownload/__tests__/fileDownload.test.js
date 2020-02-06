7b05a4029230e2e9ba2fa2cdb5f3a73b1371dc74
David Walther <walthdav@gmail.com>
2020-02-06 22:46:54 +0100

    add a download button to component

--- src/modules/data/fileDownload/__tests__/fileDownload.test.js
+++ src/modules/data/fileDownload/__tests__/fileDownload.test.js
@@ -1,5 +1,5 @@
-/* eslint-disable no-unused-vars */
 import { createElement } from 'lwc';
+import FileDownload from 'data/fileDownload';
 
 describe('download button', () => {
     afterEach(() => {
@@ -9,5 +9,26 @@ describe('download button', () => {
         }
     });
 
-    test('exists', () => {});
+    test('exists', () => {
+        /**
+         * Given
+         * -
+         */
+
+        /**
+         * When
+         * The component is added to the document without any attributes
+         */
+        const element = createElement('data-file-download', {
+            is: FileDownload
+        });
+        document.body.appendChild(element);
+
+        /**
+         * Then
+         * A Download button exits
+         */
+        const downloadBtn = element.shadowRoot.querySelector('.btn-download');
+        expect(downloadBtn).toBeTruthy();
+    });
 });
