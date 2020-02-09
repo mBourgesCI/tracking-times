31e2b2b66325a32ccc49b1fd43cd3e0a6d123a7a
David Walther <walthdav@gmail.com>
2020-02-09 19:27:41 +0100

    add filename test

--- src/modules/data/fileDownload/__tests__/fileDownload.test.js
+++ src/modules/data/fileDownload/__tests__/fileDownload.test.js
@@ -32,3 +32,40 @@ describe('download button', () => {
         expect(downloadBtn).toBeTruthy();
     });
 });
+
+describe('Attributes', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    test('filename', () => {
+        /**
+         * Given
+         * -
+         */
+
+        /**
+         * When
+         * The component is added to the document with filename attribute
+         */
+        const testValue = 'a1b2c3';
+        const element = createElement('data-file-download', {
+            is: FileDownload
+        });
+        element.filename = testValue;
+        document.body.appendChild(element);
+
+        return Promise.resolve().then(() => {
+            /**
+             * Then
+             * Given value is passed to the cmp and available
+             */
+            const testElem = document.body.querySelector('data-file-download');
+            expect(testElem).toBeTruthy();
+            expect(testElem.filename).toBe(testValue);
+        });
+    });
+});
