3801b8fff248fe70ba6d4259b070e04793903ef6
David Walther <walthdav@gmail.com>
2020-02-09 21:04:12 +0100

    transformed file download into a modal

--- src/modules/data/fileDownload/fileDownload.js
+++ src/modules/data/fileDownload/fileDownload.js
@@ -1,45 +1,26 @@
-import { LightningElement, api } from 'lwc';
+const startDownload = (filename, content, type) => {
+    doDownload(filename, content, type);
+};
 
-export default class FileDownload extends LightningElement {
-    @api
-    content;
+function doDownload(filename, content, type) {
+    //Create Blob
+    const blob = new Blob(content, { type: type });
 
-    @api
-    type;
+    // Create download URL for blob
+    const url = window.URL.createObjectURL(blob);
 
-    @api
-    filename;
+    //generate anchor for blob-url
+    const anchor = document.createElement('a');
+    anchor.href = url;
+    anchor.download = filename;
 
-    //----------------------------
-    // handlers
-    //----------------------------
+    anchor.click();
 
-    onBtnClckDownload() {
-        this.doDownload();
-    }
-
-    //----------------------------
-    // Actions
-    //----------------------------
-
-    doDownload() {
-        //Create Blob
-        const blob = new Blob(this.content, { type: this.type });
-
-        // Create download URL for blob
-        const url = window.URL.createObjectURL(blob);
-
-        //generate anchor for blob-url
-        const anchor = document.createElement('a');
-        anchor.href = url;
-        anchor.download = this.filename;
-
-        anchor.click();
-
-        // cleanup
-        anchor.remove();
-        document.addEventListener('focus', () => {
-            window.URL.revokeObjectURL(blob);
-        });
-    }
+    // cleanup
+    anchor.remove();
+    document.addEventListener('focus', () => {
+        window.URL.revokeObjectURL(blob);
+    });
 }
+
+export { startDownload };
